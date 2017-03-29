422b99f6b076f5bf1084dfbf445aa13e6d5c5635
hxh <1246996371@qq.com>
2017-03-29 20:24:16 +0800

    add repo list api

--- controllers/repo.js
+++ controllers/repo.js
@@ -1,10 +1,11 @@
 const Repo = require('../models/repo')
 module.exports = {
+
+  // 最新发布
   get_latest: (req, res) => {
     // let limit = Math.min((req.query.limit || 10), 100)
     // let skip = req.query.skip || 0
-    console.log('来请求了')
-    Repo.query({limit: 15, offset: 0, orderByRaw: 'id desc', select: ['id', 'name', 'cover', 'description_cn']}).fetchAll().then((repo) => {
+    Repo.query({limit: 15, offset: 0, orderByRaw: 'id desc', select: ['id', 'name', 'cover', 'description_cn', 'owner', 'alia']}).fetchAll().then((repo) => {
       res.send(repo)
     }).catch((err) => {
       console.error(err)
